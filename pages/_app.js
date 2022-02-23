20ef9e582468c1239128efa1315053bf5ac3f23f
Tianye Fan <t4fan@ucsd.edu>
2022-02-22 16:19:20 -0800

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import "@fontsource/montserrat-alternates"
+import "@fontsource/montserrat/700.css"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
